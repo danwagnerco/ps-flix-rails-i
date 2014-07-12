d757ea630d2c3499953aebd719d0dc9e5d3ba13d
Dan Wagner <danwagnerco@gmail.com>
2014-07-12 09:35:31 -0500

    finish day 25 the flash

--- app/controllers/movies_controller.rb
+++ app/controllers/movies_controller.rb
@@ -14,7 +14,7 @@ class MoviesController < ApplicationController
   def update
     @movie = Movie.find(params[:id])
     if @movie.update(movie_params)
-      redirect_to @movie
+      redirect_to @movie, :notice => "Movie Successfully Updated!"
     else
       render :edit
     end
@@ -27,7 +27,7 @@ class MoviesController < ApplicationController
   def create
     @movie = Movie.new(movie_params)
     if @movie.save
-      redirect_to @movie
+      redirect_to @movie, :notice => "Movie Successfully Created!"
     else
       render :new
     end
@@ -36,7 +36,8 @@ class MoviesController < ApplicationController
   def destroy
     @movie = Movie.find(params[:id])
     @movie.destroy
-    redirect_to movies_url
+    redirect_to movies_url, :notice => "Movie Successfully Deleted!"
+    # redirect_to movies_url, :danger => "I'm sorry Dave, I'm afraid I can't do that!"
   end
   
 private
--- spec/features/create_movie_spec.rb
+++ spec/features/create_movie_spec.rb
@@ -1,9 +1,8 @@
-require 'spec_helper'
+require_relative '../spec_helper'
 
 describe "Creating a new movie" do 
   it "saves the movie and shows the new event's details" do    
     visit movies_url
-    
     click_link 'Add New Movie'
     
     expect(current_path).to eq(new_movie_path)
@@ -17,12 +16,11 @@ describe "Creating a new movie" do
     fill_in "Director", with: "The ever-creative director"
     fill_in "Duration", with: "123 min"
     fill_in "Image file name", with: "movie.png"
-            
     click_button 'Create Movie'
 
     expect(current_path).to eq(movie_path(Movie.last))   
-    
     expect(page).to have_text('New Movie Title')
+    expect(page).to have_text("Movie Successfully Created!")
   end
   it "does not save the movie if it's invalid" do
     visit new_movie_url
