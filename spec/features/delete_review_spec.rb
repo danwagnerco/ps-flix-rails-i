d5d605c1bb0b71db5eb36e710df63f02026d5449
Dan Wagner <daniel.wagner@linkshare.com>
2014-07-30 15:26:57 -0500

    prepare version 1.0 for deployment

--- spec/features/delete_review_spec.rb
+++ spec/features/delete_review_spec.rb
@@ -1,16 +1,17 @@
 require_relative('../spec_helper')
 
 describe "Deleting a review" do 
-  it "destroys the review and shows the review without the deleted review" do
+  it "destroys the review and shows the index without the deleted review" do
     movie  = Movie.create(movie_attributes)
     review = movie.reviews.create(review_attributes)
 
     visit movie_path(movie)
     click_link '1 Review'
-    click_link "Delete Review"
+    click_link "Roger Ebert"
+    click_link "Delete"
 
-    expect(current_path).to eq(movie_path)
-    expect(page).not_to have_text(movie.title)
-    expect(page).to have_text("Movie Successfully Deleted!")
+    expect(current_path).to eq(movie_reviews_path(movie))
+    expect(page).not_to have_text(review.name)
+    expect(page).to have_text("Review Successfully Deleted!")
   end
 end
